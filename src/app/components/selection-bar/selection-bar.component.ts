a7598e4ec508542d9c704abfad2b3f26ba5e1976
simon.montaggioni <simon.montaggioni@gmail.com>
2019-09-29 23:44:45 -0400

    feat: Selection bar logic router implemented

--- src/app/components/selection-bar/selection-bar.component.ts
+++ src/app/components/selection-bar/selection-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-selection-bar',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectionBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
+  }
+
+  openItem( routeName: string ) {
+    this.router.navigateByUrl( `/${routeName}`);
+    console.log(routeName);
   }
 
 }
