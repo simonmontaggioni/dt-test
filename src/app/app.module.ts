831d91076b61fd292030a57569cd9df41e058dc6
simon.montaggioni <simon.montaggioni@gmail.com>
2019-09-29 23:02:22 -0400

    feat: Products and Product-detail components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TableComponent } from './components/table/table.component';
 import { SelectionBarComponent } from './components/selection-bar/selection-bar.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TableComponent,
-    SelectionBarComponent
+    SelectionBarComponent,
+    ProductDetailComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
