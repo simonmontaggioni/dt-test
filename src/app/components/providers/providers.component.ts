b3e5ef452d0557110df1904f6100ed14058ec5d9
simon.montaggioni <simon.montaggioni@gmail.com>
2019-09-30 19:32:24 -0400

    refactor: providers components refactorized to extends from generic component

--- src/app/components/providers/providers.component.ts
+++ src/app/components/providers/providers.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Provider } from 'src/app/core/provider';
 import { ProviderService } from 'src/app/services/provider.service';
+import { GenericComponent } from 'src/app/core/generic-component';
 
 @Component({
   selector: 'app-providers',
   templateUrl: './providers.component.html',
   styleUrls: ['./providers.component.css']
 })
-export class ProvidersComponent implements OnInit {
+export class ProvidersComponent extends GenericComponent implements OnInit {
 
-  itemList: Provider[];
-  itemType: Provider;
-  selectedItem: Provider;
-  action: string;
-  emptyItem: any;
 
-  constructor(private productService: ProviderService) {  }
+
+  constructor(public productService: ProviderService) {
+    super();
+    this.productService = productService;
+  }
 
   ngOnInit() {
     this.itemType = this.productService.getItemType();
@@ -23,66 +23,6 @@ export class ProvidersComponent implements OnInit {
     this.selectedItem = this.productService.getSelectedItem();
     this.emptyItem =  { name: null, type: null,  code: null , country: null };
     this.action = '';
-  }
-
-  createItem(item: Provider) {
-    this.productService.addItem(item);
-  }
-
-  updateItem(item: Provider) {
-    this.productService.updateItem(item);
-  }
-
-  deleteItem(item: Provider) {
-    alert('Are you sure to delete item id: ' + item.id);
-    this.productService.deleteItem(item);
-  }
-
-  onSelect(item: Provider) {
-    if (this.action === 'edit' || this.action === '') {
-      this.updateSeletedItem(item);
-    }
-  }
-
-  updateSeletedItem(item: Provider) {
-      this.productService.setSelectedItem(item);
-      this.selectedItem = this.productService.getSelectedItem();
-  }
-
-  saveItem(item: Provider) {
-    if (this.action === 'create') {
-      this.createItem(item);
-    } else if ( this.action === 'edit') {
-      this.updateItem(item);
-    }
-    this.setAction('');
-  }
-
-  cancelOperation(action: string) {
-    this.setAction('');
-    const item = this.emptyItem;
-    this.updateSeletedItem(item);
-  }
-
-  setAction(action: string) {
-    this.action = action;
-    if ( this.action === 'create' ) {
-
-      const item = this.emptyItem;
-      this.updateSeletedItem(item);
-
-    } else if ( this.action === 'edit' ) {
-
-      this.selectedItem = this.productService.getSelectedItem();
-
-    } else if ( this.action === 'delete' ) {
-
-      this.selectedItem = this.productService.getSelectedItem();
-      this.deleteItem(this.selectedItem);
-      this.setAction('');
-      const item = this.emptyItem;
-      this.updateSeletedItem(item);
-    }
   }
 
 }
