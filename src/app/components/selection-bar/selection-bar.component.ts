6a3be7ddabb7be4dbc865a4f71998a2261b7a5da
simon.montaggioni <simon.montaggioni@gmail.com>
2019-09-30 23:52:59 -0400

    feat: improve some styles

--- src/app/components/selection-bar/selection-bar.component.ts
+++ src/app/components/selection-bar/selection-bar.component.ts
@@ -8,12 +8,14 @@ import { Router } from '@angular/router';
 })
 export class SelectionBarComponent implements OnInit {
 
+  item = 'select an item';
   constructor(public router: Router) { }
 
   ngOnInit() {
   }
 
   openItem( routeName: string ) {
+    this.item = routeName;
     this.router.navigateByUrl( `/${routeName}`);
     console.log(routeName);
   }
